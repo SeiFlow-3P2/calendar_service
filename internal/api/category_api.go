d641224d5663a4bae115a22a8c9c5df3738d7a8f
DanniilShishkin <nochbaal@gmail.com>
2025-05-26 14:13:15 +0300

    fix rename module api

--- internal/api/category_api.go
+++ internal/api/category_api.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/SeiFlow-3P2/calendar_service/internal/service"
 
-	pb "calendar_service/pkg/proto/calendar/v1"
+	pb "github.com/SeiFlow-3P2/calendar_service/pkg/proto/calendar/v1"
 
 	"github.com/SeiFlow-3P2/calendar_service/internal/models"
 	"google.golang.org/grpc/codes"
